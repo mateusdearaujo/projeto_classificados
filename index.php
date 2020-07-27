02ac42a343c090177819d581b27cc25cec3b3637
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2020-07-27 01:18:32 -0300

    Criando página principal

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php require "pages/header.php"; ?>
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h1 class="display-4">Nós temos hoje 999 anúncios.</h1>
+        <p class="lead">E mais de 999 usuários cadastrados</p>
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+            <h3>Pesquisa Avançada</h3>
+        </div>
+        <div class="col-sm-9">
+            <h3>Últimos Anúncios</h3>
+        </div>
+    </div>
+</div>
+<?php require "pages/footer.php"; ?>
